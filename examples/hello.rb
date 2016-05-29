06501d8e51302df2e9d23b6fd3c5cdd31687fb20
akicho8 <akicho8@gmail.com>
2016-05-29 09:51:47 +0900

    example変更

--- examples/hello.rb
+++ examples/hello.rb
@@ -1,19 +1,21 @@
-# -*- coding: utf-8 -*-
 require "../lib/rasl"
 
 app = Rasl::Processor.new
 app.assemble <<-SOURCE
+; Hello, world.
+MAIN    START
         OUT     STR, LEN
         RET
-STR     DC      'Hello'
-LEN     DC      5
+STR     DC      'Hello, world.'
+LEN     DC      13
 SOURCE
 puts app.disassemble
 app.go
+app.code_dump
 # >> 0000 7001 0000    PUSH    #0000, GR1
 # >> 0002 7002 0000    PUSH    #0000, GR2
 # >> 0004 1210 000D    LAD     GR1, #000D
-# >> 0006 1220 0012    LAD     GR2, #0012
+# >> 0006 1220 001A    LAD     GR2, #001A
 # >> 0008 F000 0001    SVC     #0001
 # >> 000A 7120         POP     GR2
 # >> 000B 7110         POP     GR1
@@ -23,5 +25,17 @@ app.go
 # >> 000F 006C         DC      108    ; 'l'
 # >> 0010 006C         DC      108    ; 'l'
 # >> 0011 006F         DC      111    ; 'o'
-# >> 0012 0005         DC      5
-# >> Hello
+# >> 0012 002C         DC      44     ; ','
+# >> 0013 0020         DC      32     ; ' '
+# >> 0014 0077         DC      119    ; 'w'
+# >> 0015 006F         DC      111    ; 'o'
+# >> 0016 0072         DC      114    ; 'r'
+# >> 0017 006C         DC      108    ; 'l'
+# >> 0018 0064         DC      100    ; 'd'
+# >> 0019 002E         DC      46     ; '.'
+# >> 001A 000D         DC      13
+# >> Hello, world.
+# >> 0000: 7001 0000 7002 0000 1210 000D 1220 001A ........
+# >> 0008: F000 0001 7120 7110 8100 0048 0065 006C .....Hel
+# >> 0010: 006C 006F 002C 0020 0077 006F 0072 006C lo, worl
+# >> 0018: 0064 002E 000D                          d..
